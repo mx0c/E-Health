1f551815cecb504817e77c74094fd5924422fcba
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2018-06-06 19:10:01 +0200

    added static file serving

--- Backend/Server/Server.js
+++ Backend/Server/Server.js
@@ -3,6 +3,7 @@ var app = express()
 var router = express.Router();
 var config = require('./config.json')
 app.use(express.json())
+app.use(express.static('public'))
 
 require('./routes')(router);
 app.use('/', router);
